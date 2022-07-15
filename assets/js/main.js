943ac41432ad9dcb842421cfb593c4674c776567
Uyitech <ewansihaevans@gmail.com>
2022-07-15 22:41:53 +0100

    Modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
-let menu = document.querySelector('.menu-icon');
+// let menu = document.querySelector('.menu-icon');
 
-menu.onclick = () => {
-    menu.classList.toggle("move");
-};
+// menu.onclick = () => {
+//     menu.classList.toggle("move");
+// };
