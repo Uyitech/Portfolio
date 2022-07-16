5d88f6b93b71657a226eb79b57f5ef1e6e0efd9b
Uyitech <ewansihaevans@gmail.com>
2022-07-16 20:14:15 +0100

    Added services

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-// let menu = document.querySelector('.menu-icon');
-
-// menu.onclick = () => {
-//     menu.classList.toggle("move");
-// };
 
 
 // let skilsContent = select('.skills-content');
@@ -18,3 +13,92 @@
 //         }
 //     })
 // }
+
+
+// let menu = document.querySelector('.mobile-nav-toggle');
+
+// menu.onclick = () => {
+//     select('#navbar').classList.toggle('navbar-mobile')
+// };
+(function () {
+    "use strict";
+
+
+
+    /**
+       * Mobile nav toggle
+       */
+    on('click', '.mobile-nav-toggle', function (e) {
+        select('#navbar').classList.toggle('navbar-mobile')
+        this.classList.toggle('bi-list')
+        this.classList.toggle('bi-x')
+    })
+
+    /**
+     * Scrool with ofset on links with a class name .scrollto
+     */
+    on('click', '#navbar .nav-link', function (e) {
+        let section = select(this.hash)
+        if (section) {
+            e.preventDefault()
+
+            let navbar = select('#navbar')
+            let header = select('#header')
+            let sections = select('section', true)
+            let navlinks = select('#navbar .nav-link', true)
+
+            navlinks.forEach((item) => {
+                item.classList.remove('active')
+            })
+
+            this.classList.add('active')
+
+            if (navbar.classList.contains('navbar-mobile')) {
+                navbar.classList.remove('navbar-mobile')
+                let navbarToggle = select('.mobile-nav-toggle')
+                navbarToggle.classList.toggle('bi-list')
+                navbarToggle.classList.toggle('bi-x')
+            }
+
+            if (this.hash == '#header') {
+                header.classList.remove('header-top')
+                sections.forEach((item) => {
+                    item.classList.remove('section-show')
+                })
+                return;
+            }
+
+            if (!header.classList.contains('header-top')) {
+                header.classList.add('header-top')
+                setTimeout(function () {
+                    sections.forEach((item) => {
+                        item.classList.remove('section-show')
+                    })
+                    section.classList.add('section-show')
+
+                }, 350);
+            } else {
+                sections.forEach((item) => {
+                    item.classList.remove('section-show')
+                })
+                section.classList.add('section-show')
+            }
+
+            scrollto(this.hash)
+        }
+    }, true)
+
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+        const toggleBacktotop = () => {
+            if (window.scrollY > 100) {
+                backtotop.classList.add('active')
+            } else {
+                backtotop.classList.remove('active')
+            }
+        }
+        window.addEventListener('load', toggleBacktotop)
+        onscroll(document, toggleBacktotop)
+    }
+
+})
