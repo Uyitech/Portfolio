3097ee8fb89d653ec0a08c0b1e4ca27c5cd65628
Uyitech <ewansihaevans@gmail.com>
2022-07-16 03:23:16 +0100

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,18 @@
 // menu.onclick = () => {
 //     menu.classList.toggle("move");
 // };
+
+
+// let skilsContent = select('.skills-content');
+// if (skilsContent) {
+//     new Waypoint({
+//         element: skilsContent,
+//         offset: '80%',
+//         handler: function (direction) {
+//             let progress = select('.progress .progress-bar', true);
+//             progress.forEach((el) => {
+//                 el.style.width = el.getAttribute('aria-valuenow') + '%'
+//             });
+//         }
+//     })
+// }
