2da07c515321ae1fc248bda5b19e750f8dfa3048
Uyitech <ewansihaevans@gmail.com>
2022-07-19 03:40:33 +0100

    Added portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,3 @@
-/**
-     * Mobile nav toggle
-     */
-on('click', '.mobile-nav-toggle', function (e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-}, true)
-
-/**
- * Mobile nav dropdowns activate
- */
-on('click', '.navbar .dropdown > a', function (e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-        e.preventDefault()
-        this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-}, true)
 
 
 
