f326f288c954ef34a8b07511784df93a1293ed9a
Uyitech <ewansihaevans@gmail.com>
2022-08-23 04:55:17 +0100

    Added new features

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,6 @@
-// AUTOTYPED
-
-var typed = new Typed(".color", {
-    strings: ["Front-end Developer", "UI/UX Designer"],
-    typeSpeed: 70,
-    backSpeed: 50,
-    loop: true
-});
-
-
-
-// Nav A Menu
-const li = document.querySelectorAll(".nav-item")
-const sec = document.querySelectorAll("section")
-
-function activeMenu() {
-    let len = sec.length;
-    while (--len && window.scrollY + 200 < sec[len].offsetTop) { }
-    li.forEach(ltx => ltx.classList.remove("active"));
-    li[len].classList.add("active");
-}
-activeMenu();
-window.addEventListener("scroll", activeMenu);
-
-
-
-// BACK TO TOP
-//Get the button
-var mybutton = document.getElementById("myBtn");
+// // BACK TO TOP
+// //Get the button
+const mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
@@ -38,16 +12,38 @@ function scrollFunction() {
         mybutton.style.display = "none";
     }
 }
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-// 
+
+
+// AUTOTYPE
+var typed = new Typed(".color", {
+    strings: ["Front-end Developer", "UI/UX Designer"],
+    typeSpeed: 70,
+    backSpeed: 50,
+    loop: true
+});
+
+
+// Nav A Menu
+const li = document.querySelectorAll(".nav-item")
+const sec = document.querySelectorAll("section")
+
+function activeMenu() {
+    let len = sec.length;
+    while (--len && window.scrollY + 90 < sec[len].offsetTop) { }
+    li.forEach(ltx => ltx.classList.remove("active"));
+    li[len].classList.add("active");
+}
+activeMenu();
+window.addEventListener("scroll", activeMenu);
+
 
 //  MIXITUP PORTFOLIO 
-var mixerPortfolio = mixitup('.portfolio-container', {
+const mixerPortfolio = mixitup('.portfolio-container', {
     selectors: {
         target: '.portfolio-item'
     },
