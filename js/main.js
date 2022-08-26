0800dba8055c3271db49b40afe4577f7f732aea2
Uyitech <ewansihaevans@gmail.com>
2022-08-27 00:53:47 +0100

    Updated

--- js/main.js
+++ js/main.js
@@ -10,21 +10,6 @@ sun.onclick = function () {
     }
 }
 
-
-//  Animation on scroll
-ScrollReveal({
-    distance: '70px',
-    duration: 2000
-});
-
-ScrollReveal().reveal('.ani', { delay: 300, origin: 'left' });
-ScrollReveal().reveal('.pani', { delay: 500, origin: 'bottom' });
-ScrollReveal().reveal('.cani', { delay: 400, origin: 'bottom' });
-ScrollReveal().reveal('.sleft', { delay: 400, origin: 'bottom' });
-ScrollReveal().reveal('.sright', { delay: 400, origin: 'bottom' });
-ScrollReveal().reveal('.section-title', { delay: 400, origin: 'bottom' });
-
-
 // AUTOTYPE
 const typed = new Typed(".color", {
     strings: ["Front-end Developer", "UI/UX Designer"],
@@ -92,3 +77,17 @@ function topFunction() {
 }
 
 
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 170, // offset (in px) from the original trigger point
+    delay: 100, // values from 0 to 3000, with step 50ms
+    duration: 1600, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, // whether animation should happen only once while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+
+});
