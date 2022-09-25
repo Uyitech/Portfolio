dd36b467173840d45af40dcc95fafae28c671825
Uyitech <ewansihaevans@gmail.com>
2022-09-25 22:56:09 +0100

    Updated

--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@ AOS.init({
 // Preloader
 var myVar;
 function myFunction() {
-    myVar = setTimeout(showPage, 700);
+    myVar = setTimeout(showPage, 500);
 }
 
 function showPage() {
@@ -122,20 +122,6 @@ $(document).ready(function () {
         return false;
     })
 });
-
-
-// Start of Tawk.to Script
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/6308df1e37898912e9655e3d/1gbdaf443';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
-})();
-// End of Tawk.to Script
-
 
 // Contact form
 function sendContact() {
